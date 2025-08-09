0ae856a0d37c0d6c43ba2e4eed6ff55b75db1d78
Abhinav Dixit <dixitabhinav788@gmail.com>
2025-08-09 12:32:38 -0700

    Changes in Images and finsih work

--- js/meals.js
+++ js/meals.js
@@ -2,14 +2,14 @@ const mealData = {
   "weight-loss": [
     { 
       title: "Grilled Chicken Salad", 
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIrANMoXtMN8eRi55iwbR5eIGjTtzVnMUssg&s",
+      img: "../assests/images/SaladMeal.jpg",
       desc: "A light, nutrient-dense salad made with lean grilled chicken breast, fresh lettuce, cherry tomatoes, cucumber, and a drizzle of olive oil.",
       calories: "350 kcal",
       protein: "28g"
     },
     { 
       title: "Berry Smoothie Bowl", 
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHFIiJNFkK5gK63uNy0XWKdyT8x5WwdgL66A&s",
+      img: "../assests/images/StrawberryMeal.jpg",
       desc: "A refreshing smoothie bowl made with blueberries, strawberries, banana, and almond milk, topped with chia seeds and granola.",
       calories: "250 kcal",
       protein: "10g"
@@ -18,14 +18,14 @@ const mealData = {
   "muscle-gain": [
     { 
       title: "Steak & Sweet Potato", 
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQryeC9Q28t-zj4RHkET0pI_f0ailxLwe3npQ&s",
+      img: "../assests/images/SteakMeal.jpeg",
       desc: "Juicy grilled beef steak served with roasted sweet potatoes and steamed vegetables.",
       calories: "550 kcal",
       protein: "40g"
     },
     { 
       title: "Protein Pancakes", 
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbLrFudcSTHFIY_qAcOaIh326V-K7TAhUsxg&s",
+      img: "../assests/images/PancakeMeal.jpg",
       desc: "Fluffy oat pancakes enriched with whey protein powder, served with peanut butter and banana slices.",
       calories: "420 kcal",
       protein: "35g"
@@ -34,14 +34,14 @@ const mealData = {
   "flexibility": [
     { 
       title: "Quinoa Veggie Bowl", 
-      img: "https://www.simplyquinoa.com/wp-content/uploads/2011/09/curried-quinoa-bowls-8.jpg",
+      img: "../assests/images/VeggieMeal.jpg",
       desc: "A colorful bowl with fluffy quinoa, steamed broccoli, roasted carrots, chickpeas, and tahini dressing.",
       calories: "400 kcal",
       protein: "15g"
     },
     { 
       title: "Avocado Toast", 
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8V-Ldp9BMWrgCZjsS0TpK92i280CJoXALDg&s",
+      img: "../assests/images/AvacadoMeal.webp",
       desc: "Whole grain toast topped with mashed avocado, cherry tomatoes, and pumpkin seeds.",
       calories: "320 kcal",
       protein: "12g"
